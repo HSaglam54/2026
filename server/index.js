1629619c484b4e0e52b3e301652a897ce6bf1497
HSaglam54 <hasan.saglam@gmx.at>
2026-03-02 20:23:10 +0100

    Create index.js for server setup.

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api', require('./routes/routes'));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
